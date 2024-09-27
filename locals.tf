f7ce5553daa02a2fba068e1437f32d2ee34d4d71
Zidong Lu <zilu@microsoft.com>
2024-09-27 16:54:40 +0800

    fix

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
       nextHopIpAddress = var.default_gateway
     }
   }
-  route_0_omit_null   = { for k, v in route_0 : k => v if v != null }
+  route_0_omit_null   = { for k, v in local.route_0 : k => v if v != null }
   subnet_0_properties = { for k, v in local.subnet_0_properties_full : k => v if v != null }
   subnet_0_properties_full = {
     addressPrefix      = var.address_prefix, # compute from starting address and ending address
