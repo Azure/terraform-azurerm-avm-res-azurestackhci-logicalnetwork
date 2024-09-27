688ba1a4b85ac3a44b5846fdc656b46286e470c6
Zidong Lu <zilu@microsoft.com>
2024-09-27 16:52:07 +0800

    remove unnecessary data source

--- locals.tf
+++ locals.tf
@@ -1,6 +1,14 @@
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  subnet_0_properties                = { for k, v in local.subnet_0_properties_full : k => v if v != null }
+  route_0 = {
+    name = var.route_name
+    properties = {
+      addressPrefix    = "0.0.0.0/0",
+      nextHopIpAddress = var.default_gateway
+    }
+  }
+  route_0_omit_null   = { for k, v in route_0 : k => v if v != null }
+  subnet_0_properties = { for k, v in local.subnet_0_properties_full : k => v if v != null }
   subnet_0_properties_full = {
     addressPrefix      = var.address_prefix, # compute from starting address and ending address
     ipAllocationMethod = "Static",
@@ -13,13 +21,7 @@ locals {
     routeTable = {
       properties = {
         routes = [
-          {
-            name = var.route_name
-            properties = {
-              addressPrefix    = "0.0.0.0/0",
-              nextHopIpAddress = var.default_gateway
-            }
-          }
+          local.route_0_omit_null
         ]
       }
     }
